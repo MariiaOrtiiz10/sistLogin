6d157f56bb938a35c48c7578809a47b4ea6c4915
MariiaOrtiiz10 <mariamontewei10@gmail.com>
2024-12-18 12:27:59 +0100

    interfazLogin

--- Login/Form1.cs
+++ Login/Form1.cs
@@ -16,5 +16,15 @@ namespace Login
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Le has dado al botón aceptar");
+        }
     }
 }
